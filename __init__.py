deb5211e3c3f6eab5335e3db16ce5fe7f1e3b9aa
Marcio Granzotto <oscensores@gmail.com>
2021-04-19 14:17:40 +0000

    Change setup to use the UI Config Flow

--- __init__.py
+++ __init__.py
@@ -1,95 +1,34 @@
 """
 Support for LG Smartthinq devices.
 """
+import asyncio
+import json
 import logging
 import os
-import json
-import wideq
+from typing import Dict
+from urllib.parse import parse_qs, urlencode, urljoin, urlparse
 
-import voluptuous as vol
-
-from homeassistant.const import CONF_REGION, CONF_TOKEN
 import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+import wideq
+from homeassistant import config_entries
+from homeassistant.const import CONF_REGION, CONF_TOKEN
+from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import discovery
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.typing import HomeAssistantType
+from wideq import Client
+from wideq.core import (Auth, InvalidCredentialError, NotConnectedError,
+                        NotLoggedInError, TokenError)
 
-DOMAIN = 'thinq_v2'
+from .const import (CLIENT, CONF_LANGUAGE, CONF_OAUTH_URL, CONF_OAUTH_USER_NUM,
+                    CONF_WIDEQ_STATE, DOMAIN, LGE_DEVICES,
+                    SMARTTHINQ_COMPONENTS, STARTUP)
 
-CONF_LANGUAGE = 'language'
-CONF_WIDEQ_STATE='wideq_state'
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required(CONF_WIDEQ_STATE): cv.string
-        })
-}, extra=vol.ALLOW_EXTRA)
+_LOGGER = logging.getLogger(__name__)
 
-
-LOGGER = logging.getLogger(__name__)
-
-SMARTTHINQ_COMPONENTS = [
-    'sensor',
-    'climate',
-]
-KEY_SMARTTHINQ_DEVICES = 'smartthinq_devices'
-README_URL = 'https://github.com/marciogranzotto/hass-smartthinq/blob/master/README.md'
-
-KEY_DEPRECATED_REFRESH_TOKEN = 'refresh_token'
-KEY_DEPRECATED_COUNTRY = 'country'
-KEY_DEPRECATED_LANGUAGE = 'language'
-
-DEPRECATION_WARNING = (
-    'Direct use of the smartthinq components without a toplevel '
-    'smartthinq platform configuration is deprecated. Please use '
-    'a top-level smartthinq platform instead. Please see {readme_url} . '
-    'Configuration mapping:\n '
-    '\tclimate.{key_deprecated_token} -> {domain}.{key_token}\n'
-    '\tclimate.{key_deprecated_country} -> {domain}.{key_region}\n'
-    '\tclimate.{key_deprecated_language} -> {domain}.{key_language}').format(
-        readme_url=README_URL,
-        key_deprecated_token=KEY_DEPRECATED_REFRESH_TOKEN,
-        key_token=CONF_TOKEN,
-        key_deprecated_country=KEY_DEPRECATED_COUNTRY,
-        key_region=CONF_REGION,
-        key_deprecated_language=KEY_DEPRECATED_LANGUAGE,
-        key_language=CONF_LANGUAGE,
-        domain=DOMAIN)
-
-
-def setup(hass, config):
-    if DOMAIN not in config:
-        LOGGER.warning(DEPRECATION_WARNING)
-        return True
-
-
-    if KEY_SMARTTHINQ_DEVICES not in hass.data:
-        hass.data[KEY_SMARTTHINQ_DEVICES] = []
-
-    state_file = config[DOMAIN].get(CONF_WIDEQ_STATE)
-
-    # Load the configuration from the state file
-    try:
-        with open(state_file) as f:
-            LOGGER.debug("State file found '%s'", os.path.abspath(state_file))
-            state = json.load(f)
-    except IOError:
-        state = {}
-        LOGGER.debug("No state file found (tried: '%s')",
-                     os.path.abspath(state_file))
-
-    client = wideq.Client.load(state)
-    hass.data[CONF_WIDEQ_STATE] = state
-
-    while True:
-        try:
-            for device in client.devices:
-                hass.data[KEY_SMARTTHINQ_DEVICES].append(device.id)
-
-            for component in SMARTTHINQ_COMPONENTS:
-                discovery.load_platform(hass, component, DOMAIN, {}, config)
-            return True
-        except wideq.NotLoggedInError:
-            LOGGER.info('Session expired.')
-            client.refresh()
+KEY_SMARTTHINQ_DEVICES = "smartthinq_devices"
+README_URL = "https://github.com/marciogranzotto/hass-smartthinq/blob/master/README.md"
 
 
 class LGDevice(Entity):
@@ -104,3 +43,184 @@ class LGDevice(Entity):
     @property
     def available(self):
         return True
+
+
+class LGEAuthentication:
+    def __init__(self, region, language):
+        self._region = region
+        self._language = language
+
+    def _create_client(self):
+        client = Client(country=self._region, language=self._language)
+        return client
+
+    def getLoginUrl(self) -> str:
+
+        login_url = None
+        client = self._create_client()
+
+        try:
+            login_url = client.gateway.oauth_url()
+        except Exception:
+            _LOGGER.exception("Error retrieving login URL from ThinQ")
+
+        return login_url
+
+    def getOAuthInfoFromUrl(self, callback_url) -> Dict[str, str]:
+        oauth_info = None
+        try:
+            client = self._create_client()
+            auth = Auth.from_url(client.gateway, callback_url)
+            out = {
+                "oauth_url": auth.oauth_root,
+                "user_number": auth.user_number,
+                "access_token": auth.access_token,
+                "refresh_token": auth.refresh_token,
+            }
+            _LOGGER.info(out)
+            return out
+        except Exception:
+            _LOGGER.exception("Error retrieving OAuth info from ThinQ")
+
+        return oauth_info
+
+    def getOAuthFromUrl(self, callback_url) -> Auth:
+        try:
+            client = self._create_client()
+            return Auth.from_url(client.gateway, callback_url)
+        except Exception:
+            _LOGGER.exception("Error retrieving OAuth info from ThinQ")
+            return None
+
+    def createClientWithAuth(self, auth):
+        client = self._create_client()
+        client._auth = auth
+        client.refresh()
+        client._devices = client.session.get_devices()
+        return client
+
+    def createClientFromToken(self, token, oauth_url=None, oauth_user_num=None):
+        client = self._create_client()
+        client._auth = Auth(client.gateway, None, token, oauth_user_num, oauth_url)
+        client.refresh()
+        client._devices = client.session.get_devices()
+        return client
+
+
+async def async_setup(hass, config):
+    """
+    This method gets called if HomeAssistant has a valid configuration entry within
+    configurations.yaml.
+    Thus, in this method we simply trigger the creation of a config entry.
+    :return:
+    """
+    conf = config.get(DOMAIN)
+    hass.data[DOMAIN] = {}
+
+    if conf is not None:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": config_entries.SOURCE_IMPORT}, data=conf
+            )
+        )
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistantType, config_entry):
+    """
+    This class is called by the HomeAssistant framework when a configuration entry is provided.
+    """
+
+    refresh_token = config_entry.data.get(CONF_TOKEN)
+    region = config_entry.data.get(CONF_REGION)
+    language = config_entry.data.get(CONF_LANGUAGE)
+    oauth_url = config_entry.data.get(CONF_OAUTH_URL)
+    oauth_user_num = config_entry.data.get(CONF_OAUTH_USER_NUM)
+
+    _LOGGER.info(STARTUP)
+    _LOGGER.info(
+        "Initializing ThinQ platform with region: %s - language: %s",
+        region,
+        language,
+    )
+
+    hass.data.setdefault(DOMAIN, {})[LGE_DEVICES] = {}
+
+    # if network is not connected we can have some error
+    # raising ConfigEntryNotReady platform setup will be retried
+    lgeauth = LGEAuthentication(region, language)
+    try:
+        client = await hass.async_add_executor_job(
+            lgeauth.createClientFromToken, refresh_token, oauth_url, oauth_user_num
+        )
+
+    except InvalidCredentialError:
+        _LOGGER.error("Invalid ThinQ credential error. Component setup aborted")
+        return False
+
+    except Exception:
+        _LOGGER.warning(
+            "Connection not available. ThinQ platform not ready", exc_info=True
+        )
+        raise ConfigEntryNotReady()
+
+    if not client._devices:
+        _LOGGER.error("No ThinQ devices found. Component setup aborted")
+        return False
+
+    _LOGGER.info("ThinQ client connected")
+
+    try:
+        hass.data[CONF_WIDEQ_STATE] = client.dump()
+        await lge_devices_setup(hass, client, config_entry)
+    except Exception:
+        _LOGGER.warning(
+            "Connection not available. ThinQ platform not ready", exc_info=True
+        )
+        raise ConfigEntryNotReady()
+
+    for platform in SMARTTHINQ_COMPONENTS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(config_entry, platform)
+        )
+
+    return True
+
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    await asyncio.gather(
+        *[
+            hass.config_entries.async_forward_entry_unload(config_entry, platform)
+            for platform in SMARTTHINQ_COMPONENTS
+        ]
+    )
+
+    hass.data.pop(DOMAIN)
+
+    return True
+
+
+async def lge_devices_setup(hass, client, config_entry):
+    """Query connected devices from LG ThinQ."""
+    _LOGGER.info("Starting LGE ThinQ devices...")
+
+    device_count = 0
+
+    if KEY_SMARTTHINQ_DEVICES not in hass.data:
+        hass.data[KEY_SMARTTHINQ_DEVICES] = []
+
+    while True:
+        try:
+            for device in client.devices:
+                hass.data[KEY_SMARTTHINQ_DEVICES].append(device.id)
+
+            for component in SMARTTHINQ_COMPONENTS:
+                discovery.load_platform(hass, component, DOMAIN, {}, config_entry)
+                device_count += 1
+            _LOGGER.info("Founds %s LGE device(s)", str(device_count))
+            return
+        except wideq.NotLoggedInError:
+            _LOGGER.info("Session expired.")
+            client.refresh()
---
+++ const.py
@@ -0,0 +1,34 @@
+"""
+Support to interface with LGE ThinQ Devices.
+"""
+
+__version__ = "1.1.0"
+PROJECT_URL = "https://github.com/marciogranzotto/hass-smartthinq/"
+ISSUE_URL = "{}issues".format(PROJECT_URL)
+
+DOMAIN = "thinq_v2"
+
+CONF_LANGUAGE = "language"
+CONF_OAUTH_URL = "outh_url"
+CONF_OAUTH_USER_NUM = "outh_user_num"
+CONF_WIDEQ_STATE = "wideq_state"
+
+CLIENT = "client"
+LGE_DEVICES = "lge_devices"
+
+SMARTTHINQ_COMPONENTS = [
+    "sensor",
+    "climate",
+]
+
+STARTUP = """
+-------------------------------------------------------------------
+{}
+Version: {}
+This is a custom component
+If you have any issues with this you need to open an issue here:
+{}
+-------------------------------------------------------------------
+""".format(
+    DOMAIN, __version__, ISSUE_URL
+)
